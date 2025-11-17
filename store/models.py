672169f7d5cef1a5bcca7848e5ffef6f072b026d
jaychaudhari01 <jayavi3118@gmail.com>
2025-11-17 11:48:35 +0530

    models.py updated

--- store/models.py
+++ store/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# Apne User model mein ye fields add karo:
 class User(AbstractUser):
     phone = models.CharField(max_length=15, blank=True, null=True)
+    # NEW FIELDS - Add these:
+    address = models.TextField(blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    pincode = models.CharField(max_length=10, blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
 
     def __str__(self):
         return self.username
